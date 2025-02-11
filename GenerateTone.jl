90c17df590fc293d2748ee4fb73df24ca45e1309
Nyan Helsing <me@nyanhelsing.io>
2025-02-11 01:01:40 -0500

    make the tones recognizable hehe

--- GenerateTone.jl
+++ GenerateTone.jl
@@ -2,8 +2,8 @@ using PortAudio
 
 # Audio parameters
 const SAMPLE_RATE = 44100           # Samples per second
-const FREQUENCY = 2000.0            # Frequency in Hertz
-const DURATION = 2.0                # Duration in seconds
+const FREQUENCY = 1000.0            # Frequency in Hertz
+const DURATION = 0.666                # Duration in seconds
 
 const MASTER_OUTPUT = 0.75               # Amplitude (range: 0.0 to 1.0)
 
@@ -12,6 +12,13 @@ amp_env = exp.(DECAY * (1:DURATION*SAMPLE_RATE) / SAMPLE_RATE)
 
 waveform = sin.(2pi * FREQUENCY * (1:DURATION*SAMPLE_RATE) / SAMPLE_RATE) .* amp_env * MASTER_OUTPUT
 
-PortAudioStream(0, 2; samplerate=SAMPLE_RATE, latency=0.1) do stream
+PortAudioStream("pulse"; samplerate=SAMPLE_RATE, latency=0.1) do stream
+    waveform = sin.(2pi * FREQUENCY * 1 * (1:DURATION*SAMPLE_RATE) / SAMPLE_RATE) .* amp_env * MASTER_OUTPUT
+    write(stream, waveform)
+
+    waveform = sin.(2pi * (FREQUENCY * 1.51) * (1:DURATION*SAMPLE_RATE) / SAMPLE_RATE) .* amp_env * MASTER_OUTPUT
+    write(stream, waveform)
+
+    waveform = sin.(2pi * FREQUENCY * 1.24 * (1:DURATION*SAMPLE_RATE) / SAMPLE_RATE) .* amp_env * MASTER_OUTPUT
     write(stream, waveform)
 end
